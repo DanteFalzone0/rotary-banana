309e6d86af8885f117afbf9a9c7df36f92f615ce
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-03-31 02:31:40 -0500

    Add files via upload

---
+++ banana.hpp
@@ -0,0 +1,105 @@
+/*
+    banana.hpp - a C++ library written by Dante Falzone for displaying
+    animated ascii art in the terminal. Inspired by javidx9's olcConsoleGameEngine.
+
+    Copyright (C) 2019  Dante James Falzone
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    You can contact Dante at the following spambot-proofed email address:
+                                   _                           ___
+      |          |           .    /      |                    / _ \                 |                       . |
+     _|  __   _  |__  __         |_  __  | __  __   _   __   | / | |   __   _   __  |__  __   _   ___   __    |    __  __   ___
+    / | /  | | | |   /__\    |   |  /  | |  / /  \ | | /__\  | \_|/   |  \ | \ /  \ |   /  \ | | | | | /  | | |   /   /  \ | | |
+    \_| \_/| | |  \_ \__  .  | . |  \_/| | /_ \__/ | | \__    \___    |__/ |   \__/  \_ \__/ | | | | | \_/| | | . \__ \__/ | | |
+                            _/                                        |
+*/
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cstring>
+
+using namespace std::this_thread;
+using namespace std::chrono;
+using namespace std;
+
+
+// 2d array to store the frame's contents.
+char frame_contents[20][40] = {
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '}
+};
+
+// Function to refresh the frame.
+void fresh_frame(void) {
+    // Go back up the terminal display to print out the frame.
+    for (int b = 0; b < 22; b++) {
+        cout << "\x1b[A";
+    }
+
+    // Print the topmost edge of the frame.
+    cout << "    +";
+    for (int i = 0; i < 40; i++) {
+        cout << "-";
+    }
+    cout << "+    \n";
+
+    // Print all the rows of the frame.
+    for (int y = 0; y < 20; y++) {
+        cout << "    |";
+        for (int x = 0; x < 40; x++) {
+            cout << frame_contents[y][x];
+        }
+        cout << "|    \n";
+    }
+
+    // Print the bottommost edge of the frame.
+    cout << "    +";
+    for (int y = 0; y < 40; y++) {
+        cout << "-";
+    }
+    cout << "+    \n";
+}
+
+
+// Function to update a frame cell.
+void blit(char glyph, int x, int y) {
+    frame_contents[y][x] = glyph;
+}
+
+// Function to wait a certain number of milliseconds before
+// refreshing the frame.
+void advance_frame(int ms) {
+    fresh_frame();
+    sleep_for(milliseconds(ms));
+}
---
+++ game0.cpp
@@ -0,0 +1,12 @@
+#include "banana.hpp"
+
+int main(void) {
+    for (int i = 0; i < 20; i++) {
+        blit('X', (i * 2), i);
+        if (i != 0) {
+            blit(' ', ((i - 1) * 2), (i - 1));
+        }
+        advance_frame(100);
+    }
+    return 0;
+}
