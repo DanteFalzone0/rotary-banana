df96015c04a6c16601b4be90ae679f4a76b1cdb7
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-03-31 07:03:43 -0500

    Add files via upload

---
+++ lineanim.cpp
@@ -0,0 +1,30 @@
+#include "banana.hpp"
+
+int main(void) {
+    int x = 18;
+    int y = 2;
+    while (1 > 0) {
+        line(x, y, 18, 10, '*');
+        if ((x <= 18) && (y <= 10)) {
+            x-=2;
+            y++;
+        } else if ((x <= 18) && (y > 10)) {
+            x+=2;
+            y++;
+        } else if ((x > 18) && (y > 10)) {
+            x+=2;
+            y--;
+        } else if ((x > 18) && (y <= 10)) {
+            x-=2.3;
+            y--;
+            if (y < 2) {
+                y = 2;
+            }
+            if (x < 18) {
+                exit(EXIT_SUCCESS);
+            }
+        }
+        advance_frame(100);
+    }
+    return 0;
+}
