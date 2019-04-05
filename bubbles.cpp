a6aeb3e04e20aca77616fd7953f9e52d99780a9c
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-04-05 12:31:56 -0500

    Program that draws bubbles

---
+++ bubbles.cpp
@@ -0,0 +1,27 @@
+#include "banana.hpp"
+#include <random>
+
+int randomize(int upper_bound) {
+    random_device rd;
+    uniform_int_distribution<int> dist(0, upper_bound);
+    return dist(rd);
+}
+
+
+int main() {
+    int bubbles_x[6] = {2,  6,  12, 18, 25, 34};
+    int bubbles_y[6] = {10, 25, 5, 28, 2, 30};
+    while (1 == 1) {
+        initialize();
+        for (int i = 0; i < 6; i++) {
+            draw_circle(bubbles_x[i], bubbles_y[i], 4, '#');
+            bubbles_y[i]--;
+            if (bubbles_y[i] < -5) {
+                bubbles_y[i] = (23 + randomize(12));
+                bubbles_x[i] = randomize(40);
+            }
+        }
+        advance_frame(300);
+    }
+    return 0;
+}
